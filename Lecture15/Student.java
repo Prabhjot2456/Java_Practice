3564a799d31cfffec8448b87a666ca7c4bb74659
Prabhjot Singh <singhprabhjot.prab@gmail.com>
2026-07-30 19:05:53 +0530

    inheritance

---
+++ Lecture15/Student.java
@@ -0,0 +1,31 @@
+// Inhertitance 
+
+// class Student{
+
+//     int id; 
+//     String name;
+
+//     void display(){
+//         System.out.println(id);
+//         System.out.println(name);
+//     }
+//     public static void main(String[] args) {
+        
+//     }
+// }
+
+
+// class Hosteler extends Student{
+//     String hostelName;
+
+//     public static void main(String[] args) {
+//         Hosteler h1 = new Hosteler();
+
+//     }
+// }
+// hosteler object contains two parts inherited part and own part
+
+
+
+// method overiding : when there are two matching methods in 2 classes which are in parent and class relation
+// ambiguity : it is same like overriding but there are two different classes in this (multiple inheritance)
