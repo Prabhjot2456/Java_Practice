d132f57da0ef36e875fcf818b110c878dae81a3b
Prabhjot Singh <singhprabhjot.prab@gmail.com>
2026-08-06 21:12:17 +0530

    Lecture 17, 18

---
+++ Lecture17/Lecture17.java
@@ -0,0 +1,11 @@
+// Polymorphism : 
+// dynamic method dispatch
+// reference type, reference variable
+
+
+
+// type casting for non primitive or for class type
+// Hosteller h1 = (Hosteller)s1;
+
+// upcasting --> Student s1 = new Hosteller();       // widening(because of scope)
+ 
---
+++ Lecture18/Lecture18.java
@@ -0,0 +1,9 @@
+class Lecture18{
+    public static void main(String[] args){
+
+    }
+}
+
+
+// in abstract class fxn declaration can be done 
+// concrete method har method jiski difinition hai  
